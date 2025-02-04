5027243f0eaa37c6c6533323e8822b69eedd778d
kaavyas11 <ks125@wellesley.edu>
2025-02-04 15:50:54 -0500

    Update uniquelinks.py
    Adding domain type column

--- uniquelinks.py
+++ uniquelinks.py
@@ -47,10 +47,12 @@ print(len(common))
 
 #creates the first row of the csv
 table = [
-    ["Domain","Url","Url occurences","Common?"]
+    ["Domain","Url","Url occurences","Common?","Domain Type"]
 ]
 
 #writes a list of lists for the english csv including new information for all of the links from the top 50 domains
+with open ("us-news-domains-v2.0.0.csv","r",newline="") as f:
+        newslinks = list(csv.DictReader(f))
 new = []
 for dom in domainEng:
     counted = Counter([row['Article Link'] for row in fixedEng if row['fixedDomain'] == dom])
@@ -62,12 +64,27 @@ for dom in domainEng:
             new.append("yes")
         else:
             new.append("no")
+        if ".gov" in dom or "ma.us" in dom or "co.us" in dom or "tx.us" in dom or "ncsl" in dom:
+            new.append("Government")
+        if "bexar" in dom:
+            new.append("County")
+        if "youtube" in dom:
+            new.append("Social Media")
+        if "facebook" in dom:
+            new.append("Social Media")
+        for line in newslinks:
+            if line['domain'] in dom:
+                new.append("News")
+        if "kff" in dom or "wikipedia" in dom or "vote.org" in dom or "usvotefoundation" in dom or "ballotpedia" in dom:
+            new.append("Nonprofit")
+        if "vote411" in dom or "nycvotes" in dom or "270" in dom:
+            new.append("Organization")
         table.append(new)
         new = []
 
 #writes a list of lists for the Spanish csv including new information for all of the links from the top 50 domains
 tableS = [
-    ["Domain","Url","Url occurences","Common?"]
+    ["Domain","Url","Url occurences","Common?", "Domain Type"]
 ]
 newS = []
 for dom in domainSp:
@@ -80,6 +97,30 @@ for dom in domainSp:
             newS.append("yes")
         else:
             newS.append("no")
+        tableS.append(newS)
+        if ".gov" in dom or "ma.us" in dom or "co.us" in dom or "tx.us" in dom or "ncsl" in dom:
+            newS.append("Government")
+        if  "bexar" in dom  or "harris" in dom or "ocf" in dom or "epc" in dom or "sdvote" in dom or "sbcounty" in dom:
+            newS.append("County")
+        if "youtube" in dom:
+            newS.append("Social Media")
+        if "facebook" in dom or "instagram" in dom:
+            newS.append("Social Media")
+        for line in newslinks:
+            if line['domain'] in dom:
+                newS.append("News")
+        if "yahoo" in dom or "infobae" in dom or "voz" in dom or "us.as" in dom:
+            newS.append("News")
+        if "kff" in dom or "wikipedia" in dom or "vote.org" in dom or "brennan" in dom or "ballotpedia" in dom or "calmatters" in dom:
+            newS.append("Nonprofit")
+        if "vote411" in dom or "nycvotes" in dom or "270" in dom:
+            newS.append("Organization")
+        if ".mx" in dom:
+            newS.append("Mexico")
+        if "amazon" in dom:
+            newS.append("Commercial")
+        if ".cl" in dom:
+            newS.append("Chile")
         tableS.append(newS)
         newS = []
 
